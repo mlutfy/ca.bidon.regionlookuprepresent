340309c583d444c2286a31f771d05f3dfc6513f1
Mathieu Lutfy <mathieu@symbiotic.coop>
2019-03-20 13:24:24 -0400

    ccua/civicrm#131 Many fixes to api/v3/Regionlookuprepresent/Batchridingupdatebygeo, a lot of un-hardcoding.

--- api/v3/Regionlookuprepresent/Batchridingupdatebygeo.php
+++ api/v3/Regionlookuprepresent/Batchridingupdatebygeo.php
@@ -1,19 +1,82 @@
 <?php
 
-function civicrm_api3_regionlookuprepresent_ridinglookup($params) {
-  // FIXME: 3=Head office
-  // FIXME lots of hardcoded stuff.
-  $sql = 'SELECT c.id as contact_id, a.*, info.federal_riding_64 as federal_riding
+/**
+ * Updates the riding information for contacts (organisations) that have
+ * been already geolocated (based on their address), but not associated
+ * with a riding.
+ */
+function civicrm_api3_regionlookuprepresent_batchridingupdatebygeo($params) {
+  // Exclude 'riding' contact subtypes.
+  // I'm not really sure why, to be honest. It's not a big deal?
+  $excludes = [];
+  if ($t = Civi::settings()->get('regionlookuprepresent_federalriding_ctype')) {
+    $subtype_name = civicrm_api3('ContactType', 'getvalue', [
+      'id' => $t,
+      'return' => 'name',
+    ]);
+    $excludes[] = $subtype_name;
+  }
+  if ($t = Civi::settings()->get('regionlookuprepresent_provincialriding_ctype')) {
+    $subtype_name = civicrm_api3('ContactType', 'getvalue', [
+      'id' => $t,
+      'return' => 'name',
+    ]);
+    $excludes[] = $subtype_name;
+  }
+
+  // Fetch the custom field for the federal riding
+  $federalriding_cfid = Civi::settings()->get('regionlookuprepresent_federalriding_cfid');
+
+  if (empty($federalriding_cfid)) {
+    throw new Exception("The Federal Custom Field is not set. Please check the settings.");
+  }
+
+  $cf = civicrm_api3('CustomField', 'getsingle', [
+    'id' => $federalriding_cfid,
+    'api.CustomGroup.get' => [],
+  ]);
+
+  $federalriding_cf_table = $cf['api.CustomGroup.get']['values'][0]['table_name'];
+  $federalriding_cf_field = $cf['column_name'];
+
+  // Fetch the custom field for the provincial riding
+  $provincialriding_cfid = Civi::settings()->get('regionlookuprepresent_provincialriding_cfid');
+
+  if (empty($provincialriding_cfid)) {
+    throw new Exception("The Federal Custom Field is not set. Please check the settings.");
+  }
+
+  $cf = civicrm_api3('CustomField', 'getsingle', [
+    'id' => $provincialriding_cfid,
+    'api.CustomGroup.get' => [],
+  ]);
+
+  $provincialriding_cf_table = $cf['api.CustomGroup.get']['values'][0]['table_name'];
+  $provincialriding_cf_field = $cf['column_name'];
+
+  // Location type for the address to lookup
+  $contact_location_type_id = Civi::settings()->get('regionlookuprepresent_contact_loctype_id');
+
+  if (empty($contact_location_type_id)) {
+    throw new Exception("The Contact Location Type to lookup is not set. Please check the settings.");
+  }
+
+  $sql = 'SELECT c.id as contact_id, a.*, ' . $federalriding_cf_field . ' as federal_riding, prov.abbreviation
     FROM civicrm_contact c
-    INNER JOIN civicrm_address a ON (a.contact_id = c.id AND a.location_type_id = 3)
-    LEFT JOIN civicrm_value_cmc_organisation_information_1 info ON (info.entity_id = c.id)
+    INNER JOIN civicrm_address a ON (a.contact_id = c.id AND a.location_type_id = ' . $contact_location_type_id . ')
+    LEFT JOIN ' . $federalriding_cf_table . ' as info ON (info.entity_id = c.id)
+      ' . ($federalriding_cf_table != $provincialriding_cf_table ? " LEFT JOIN $provincialriding_cf_table as info2 ON (info2.entity_id = c.id) " : '') . '
+    LEFT JOIN civicrm_state_province prov ON (prov.id = a.state_province_id)
     WHERE c.contact_type = "Organization"
-      AND c.contact_sub_type IS NULL
-      AND info.federal_riding_64 IS NULL
-      AND geo_code_1 IS NOT NULL';
+      ' . (!empty($excludes) ? ' AND (c.contact_sub_type NOT IN("' . implode('","', $excludes) . '") OR c.contact_sub_type IS NULL)' : '') . '
+      AND (' . $federalriding_cf_field . ' IS NULL OR ' . $provincialriding_cf_field . ' IS NULL)
+      AND geo_code_1 IS NOT NULL
+      AND (street_address IS NOT NULL OR postal_code IS NOT NULL)
+      AND c.is_deleted = 0';
 
   $sqlparams = [];
 
+  // This is mostly for testing purposes
   if (!empty($params['contact_id'])) {
     $sql .= ' AND c.id = %1';
     $sqlparams[1] = [$params['contact_id'], 'Positive'];
@@ -27,8 +90,11 @@ function civicrm_api3_regionlookuprepresent_ridinglookup($params) {
       $result = file_get_contents('https://represent.opennorth.ca/boundaries/?contains=' . $geo);
       $data = json_decode($result, TRUE);
 
+      $found_federal = FALSE;
+      $found_provincial = FALSE;
+
       foreach ($data['objects'] as $boundary) {
-        if ($boundary['boundary_set_name'] == 'Federal electoral district') {
+        if (!$found_federal && $boundary['boundary_set_name'] == 'Federal electoral district') {
           // I could not find an easy way to distinguish the result for a
           // pas boundary, vs current boundary, except by looking at the URL.
           // Ex: /boundaries/federal-electoral-districts/62001/
@@ -37,24 +103,49 @@ function civicrm_api3_regionlookuprepresent_ridinglookup($params) {
             continue;
           }
 
-          $result2 = civicrm_api3('Contact', 'get', [
-            'contact_sub_type' => 'Federal_riding',
-            'organization_name' => $boundary['name'],
-            'sequential' => 1,
-          ]);
+          $found_federal = TRUE;
 
-          if ($result2['count']) {
-            $riding_contact_id = $result2['values'][0]['contact_id'];
+          $riding_contact_id = CRM_Regionlookuprepresent_BAO_Riding::findRiding($boundary['name'], 'Federal_riding');
 
+          if ($riding_contact_id) {
             civicrm_api3('Contact', 'create', [
               'contact_id' => $dao->contact_id,
-              'custom_64' => $riding_contact_id,
+              'custom_' . $federalriding_cfid => $riding_contact_id,
             ]);
 
             Civi::log()->info($dao->contact_id . ': [' . $geo . '] Updated riding to ' . $riding_contact_id . ' (' . $boundary['name'] . ')');
           }
           else {
-            Civi::log()->warning($dao->contact_id . ': [' . $geo . '] Could not find the riding for: ' . $boundary['name'] . ' -- ' . $boundary['url']);
+            Civi::log()->warning($dao->contact_id . ': [' . $geo . '] Could not find the federal riding for: ' . $boundary['name'] . ' -- ' . $boundary['url']);
+          }
+        }
+        elseif (!$found_provincial && strpos($boundary['boundary_set_name'], 'electoral district')) {
+          // Ex: Ontario electoral district
+          // Here we don't have an easy way of finding the current boundary. We just assume the first result is the latest.
+          $found_provincial = TRUE;
+
+          $abbrev = $dao->abbreviation;
+
+          // This is to match suffixes in Job/Regionlookuprepresentupdateprovincialridings.php
+          if ($abbrev == 'PE') {
+            $abbrev = 'PEI';
+          }
+          elseif ($abbrev == 'NL') {
+            $abbrev = 'NFL';
+          }
+
+          $riding_contact_id = CRM_Regionlookuprepresent_BAO_Riding::findRiding($boundary['name'] . ' [' . $abbrev . ']', 'Provincial_riding');
+
+          if ($riding_contact_id) {
+            civicrm_api3('Contact', 'create', [
+              'contact_id' => $dao->contact_id,
+              'custom_' . $provincialriding_cfid => $riding_contact_id,
+            ]);
+
+            Civi::log()->info($dao->contact_id . ': [' . $geo . '] Updated riding to ' . $riding_contact_id . ' (' . $boundary['name'] . ')');
+          }
+          else {
+            Civi::log()->warning($dao->contact_id . ': [' . $geo . '] Could not find the provincial riding for: ' . $boundary['name'] . ' [' . $abbrev . '] -- ' . $boundary['url']);
           }
         }
       }
